b3addf13da45cd15909cd17f519ec48645cbaf19
Xave Senduk <fransiscusxave@gmail.com>
2020-12-20 20:29:13 +0800

    Aktivasi login

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -37,5 +37,7 @@ class Filters extends BaseConfig
 	// List filter aliases and any before/after uri patterns
 	// that they should run on, like:
 	//    'isLoggedIn' => ['before' => ['account/*', 'profiles/*']],
-	public $filters = [];
+	public $filters = [
+		'login' => ['before' => ['/*']],
+	];
 }
