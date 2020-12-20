2080732174a35d9cfde07c87aeb7983e9f862f83
Xave Senduk <fransiscusxave@gmail.com>
2020-12-20 20:32:16 +0800

    Update akses model

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,5 +8,5 @@ class UserModel extends Model
 {
     protected $table = 'users';
     protected $useTimestamps = true;
-    protected $allowedFields = ['active'];
+    protected $allowedFields = ['id', 'email', 'username', 'password_hash', 'reset_hash', 'reset_at', 'reset_expires', 'activate_hash', 'status', 'status_message', 'active', 'force_pass_reset'];
 }
